45badcbe45f563bf930dd453095e8ba3e43ccb63
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2022-10-13 10:13:18 +0200

    feladat

---
+++ TésKszámító.py
@@ -0,0 +1,18 @@
+#print("Adja meg a téglalap a oldalát!")
+#a = int(input())
+#print("Adja meg a téglalap b oldalát!")
+#b = int(input())
+#T = a+b+a+b
+#K = a*b
+#while T < 100:
+#    print("A téglalap területe",T)
+#    print("A téglalap kerülete",K)
+a = 0
+b = 0
+T = 0
+
+while T<100:
+    a = int(input())
+    b = int(input())
+    T = a*b
+    print("A terület",T)
