365fada58dd824ef1b18549927f197f82e42e493
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2022-11-03 08:35:03 +0100

    feladat

---
+++ FoszlerDanielAlexanderdoga002.py
@@ -0,0 +1,48 @@
+a = 1
+#szam = int(input())
+
+#logikai = True
+
+#for i in range(2,szam,1):
+#    if szam % i == 0:
+#        logikai = False
+
+#if logikai:
+#    print("prímszám")
+#else:
+#    print("nem prímszám")
+
+
+a = int(input("Írja ide a bekérendő számot:"))
+logikai = True
+for i in range(2,a,1):
+    if a % i == 0:
+        logikai = False
+if logikai == False:
+    if a%2==0:
+        print("Ура(Hurrá)!")
+    elif a%3==0:
+        print("Három a magyar igazság.")
+    if a <=10:
+        if a == 1:
+            print("I")
+        elif a == 2:
+            print("II")
+        elif a == 3:
+            print("III")
+        elif a == 4:
+            print("IV")
+        elif a == 5:
+            print("V")
+        elif a == 6:
+            print("VI")
+        elif a == 7:
+            print("VII")
+        elif a == 8:
+            print("VIII")
+        elif a == 9:
+            print("IX")
+        elif a == 10:
+            print("X")
+    else:
+        print("Nem nyert!")
