ec212ef83ce7687d945abaaaa42db00aae4bf92d
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2022-11-24 09:31:56 +0100

    feladat

--- listaksz.py
+++ listaksz.py
@@ -1,6 +1,6 @@
 nev = ["Cony","Arnold Svárcenegger","Czigány Dezdemóna","Lakatos Dzsiglipuff"]
 
-szul_ev = [2006,1988,2005,1999]
+szul_ev = [1206,88,1405,1999]
 
 kor = []
 
@@ -8,3 +8,18 @@ for i,j in zip (nev,szul_ev):
     print(i,"  ",j)
     print(i, "  ",2022-j)
     kor.append(2022 - j)
+print(kor)
+#legöregebb
+
+legoregebb = kor[0]
+for i in kor:
+    if i > legoregebb:
+        legoregebb = i
+print(legoregebb)
+
+#legfiatalabb
+legfiatalabb = kor[0]
+for i in kor:
+    if i < legfiatalabb:
+        legfiatalabb = i
+print(legfiatalabb)
