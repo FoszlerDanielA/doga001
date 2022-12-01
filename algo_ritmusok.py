7e4412a70686cb6fd3ba7203cbde5e338246f4ea
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2022-12-01 09:09:33 +0100

    átírt2

--- algo_ritmusok.py
+++ algo_ritmusok.py
@@ -42,13 +42,23 @@
 #
 #print("negatív számok:", count)
 ######################################
+#t = [3,8,2,4,5,1,6]
+#n = len(t)
+#ker = 5
+#i = 0
+#while i < n and t[i] != ker:
+#    i = i + 1
+#if i<n:
+#    print("Van ilyen:",ker)
+#else:
+#    print("Nincs ilyen elem",ker)
+####################################################
 t = [3,8,2,4,5,1,6]
 n = len(t)
 ker = 5
+
 i = 0
-while i < n and t[i] != ker:
+while t[i] != ker:
     i = i + 1
-if i<n:
-    print("Van ilyen:",ker)
-else:
-    print("Nincs ilyen elem",ker)
+
+print("Hányadik helyen van:",i+1)
