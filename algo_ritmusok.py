b611eac346d141689c8cd64b6a42c64f4e29dfe6
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2022-12-01 09:22:26 +0100

    atirt3

--- algo_ritmusok.py
+++ algo_ritmusok.py
@@ -53,12 +53,26 @@
 #else:
 #    print("Nincs ilyen elem",ker)
 ####################################################
-t = [3,8,2,4,5,1,6]
-n = len(t)
-ker = 5
+#t = [3,8,2,4,5,1,6]
+#n = len(t)
+#ker = 5
+#
+#i = 0
+#while t[i] != ker:
+#    i = i + 1
+#
+#print("Hányadik helyen van:",i+1)
+####################################################
+a = [3,8,2,4,5,1,6]
+b = []
+c = []
 
-i = 0
-while t[i] != ker:
-    i = i + 1
+for elem in a:
+    if elem < 5:
+        b.append(elem)
+    else:
+        c.append(elem)
 
-print("Hányadik helyen van:",i+1)
+print(b)
+print(c)
+####################################################
