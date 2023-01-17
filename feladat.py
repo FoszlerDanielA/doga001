2b5a089c5cfaa9a6c1fa2fd8200059e02ceb3ff5
FoszlerDanielA <114134258+FoszlerDanielA@users.noreply.github.com>
2023-01-17 16:18:53 +0100

    Update feladat.py
    A megoldott feladat

--- feladat.py
+++ feladat.py
@@ -1,15 +1,15 @@
-temperatures = []
+hofok = []
 
 for i in range(1, 31):
-    temp = float(input("Enter the temperature for day " + str(i) + ": "))
-    temperatures.append(temp)
+    ho = float(input("Enter the temperature for day " + str(i) + ": "))
+    hofok.append(ho)
 
-average = sum(temperatures) / len(temperatures)
-min_temp = min(temperatures)
-min_day = temperatures.index(min_temp) + 1
-max_temp = max(temperatures)
-max_day = temperatures.index(max_temp) + 1
+atlag = sum(hofok) / len(hofok)
+min_ho = min(hofok)
+min_nap = hofok.index(min_ho) + 1
+max_ho = max(hofok)
+max_nap = hofok.index(max_ho) + 1
 
-print("Average temperature:", average)
-print("Minimum temperature of", min_temp, "was on day", min_day)
-print("Maximum temperature of", max_temp, "was on day", max_day)
+print("Átlagos hőfok:", atlag,"C°")
+print("Minimum hőfok", min_ho, "C° volt a", min_napnap,". napon)
+print("Maximum hőfok", max_ho, "C° volt a", max_nap". napon)
