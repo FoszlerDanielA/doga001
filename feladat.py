9f0eb8eaefa8187d7334696776100b1438570f5e
FoszlerDanielA <114134258+FoszlerDanielA@users.noreply.github.com>
2023-01-18 20:53:38 +0100

    Update feladat.py

--- feladat.py
+++ feladat.py
@@ -4,12 +4,12 @@ for i in range(1, 31):
     ho = float(input("Enter the temperature for day " + str(i) + ": "))
     hofok.append(ho)
 
-atlag = sum(hofok) / len(hofok)
+avg = sum(hofok) / len(hofok)
 min_ho = min(hofok)
 min_nap = hofok.index(min_ho) + 1
 max_ho = max(hofok)
 max_nap = hofok.index(max_ho) + 1
 
-print("Átlagos hőfok:", atlag,"C°")
+print("Átlagos hőfok:", avg,"C°")
 print("Minimum hőfok", min_ho, "C° volt a", min_nap,". napon")
 print("Maximum hőfok", max_ho, "C° volt a", max_nap,". napon")
