0875eb99a6e7fdb542a18e0cd9ce850319856615
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-01-19 08:17:24 +0100

    2023.01.19  Eredetileg házi

--- 30ashofok.py
+++ 30ashofok.py
@@ -9,7 +9,13 @@ min_ho = min(hofok)
 min_nap = hofok.index(min_ho) + 1
 max_ho = max(hofok)
 max_nap = hofok.index(max_ho) + 1
+ice = []
+for i in hofok:
+    if i < 0:
+        ice.append(i)
+    fd = len(ice)
 
 print("Átlagos hőfok:", avg,"C°")
 print("Minimum hőfok", min_ho, "C° volt a", min_nap,". napon")
 print("Maximum hőfok", max_ho, "C° volt a", max_nap,". napon")
+print(fd,"napnyi fagy volt")
