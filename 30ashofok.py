7fa2db3c0ad8b949368e63cba5d34f23dafa9f6e
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-01-19 08:10:12 +0100

    2023.01.19  Eredetileg házi

---
+++ 30ashofok.py
@@ -0,0 +1,15 @@
+hofok = []
+
+for i in range(1, 31):
+    ho = float(input("adja meg a hőfokát a(z) " + str(i) + ". napnak: "))
+    hofok.append(ho)
+
+avg = sum(hofok) / len(hofok)
+min_ho = min(hofok)
+min_nap = hofok.index(min_ho) + 1
+max_ho = max(hofok)
+max_nap = hofok.index(max_ho) + 1
+
+print("Átlagos hőfok:", avg,"C°")
+print("Minimum hőfok", min_ho, "C° volt a", min_nap,". napon")
+print("Maximum hőfok", max_ho, "C° volt a", max_nap,". napon")
