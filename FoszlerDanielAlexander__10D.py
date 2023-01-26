f851d2dc155f417d86610e550533eaa56488bbca
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-01-26 08:26:32 +0100

    2023.01.26  rendezések listában, min és max

---
+++ FoszlerDanielAlexander__10D.py
@@ -0,0 +1,23 @@
+lista=[10,-2,1,25,22,30,60,-99,-1252,3000,2123,16,12]
+lista1=[10,-2,1,25,22,30,60,-99,-1252,3000,2123,16,12]
+for i in range(len(lista)-1):
+    mind = i
+
+    for j in range(i + 1, len(lista)):
+        if lista[j] < lista[mind]:
+            mind = j
+        if i != mind:
+            lista[i], lista[mind] = lista[mind], lista[i]
+
+for i in range(len(lista1)-1):
+    mind = i
+
+    for j in range(i + 1, len(lista1)):
+        if lista1[j] > lista1[mind]:
+            mind = j
+        if i != mind:
+            lista1[i], lista1[mind] = lista1[mind], lista1[i]
+print(lista)
+print(lista1)
+print("legkisebb érték",lista[0])
+print("legnagyobb érték",lista[12])
