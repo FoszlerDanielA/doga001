590724acccfa6c33cd9e6ee2ee378c304e065ea0
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-02-09 08:38:34 +0100

    2023.02.09. Doga, novekvőbe rendezés függvénnyel

--- Foszler_Daniel_A_10D.py
+++ Foszler_Daniel_A_10D.py
@@ -2,9 +2,10 @@ def rendeznov(lista):
     for i in range(len(lista), 0, -1):
         for j in range(0, i - 1):
             if lista[j] > lista[j+1]:
-                a = lista[j+1]
-                lista[j + 1] = lista[j]
-                lista[j] = a
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+#                a = lista[j+1]
+#                lista[j + 1] = lista[j]
+#                lista[j] = a
     return lista
 
 lista = [1, 9, -6, 11, 7, 12, 120, -96, 55, 42, 300, 15, -1]
