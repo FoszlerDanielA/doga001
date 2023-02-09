33166a78aa7631010e9ef60272d26507726056b3
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-02-09 09:18:54 +0100

    2023.02.09. Doga, novekvőbe rendezés, min és maxkeres függvénnyel

--- Foszler_Daniel_A_10D.py
+++ Foszler_Daniel_A_10D.py
@@ -7,10 +7,22 @@ def rendeznov(lista):
 #                lista[j + 1] = lista[j]
 #                lista[j] = a
     return lista
+def minkeres(lista):
+    minimum = lista[0]
+    for i in lista:
+        if i < minimum:
+            minimum = i
+    return minimum
+def maxkeres(lista):
+    maximum = lista[0]
+    for i in lista:
+        if i > maximum:
+            maximum = i
+    return maximum
 
 lista = [1, 9, -6, 11, 7, 12, 120, -96, 55, 42, 300, 15, -1]
 lista = rendeznov(lista)
-legnagy = lista[12]
-legkis = lista[0]
-print("Legkisebb szám értéke:", legnagy)
-print("Legnagyobb szám értéke", legkis)
+legnagy = maxkeres(lista)
+legkis = minkeres(lista)
+print("Legkisebb szám értéke:", legkis)
+print("Legnagyobb szám értéke", legnagy)
