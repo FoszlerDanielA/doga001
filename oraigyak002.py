0cc0d6bd340583b66b7eb81a15aed68898735e52
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-02-16 10:26:52 +0100

    2023.02.16 megfordítás

--- oraigyak002.py
+++ oraigyak002.py
@@ -1,6 +1,14 @@
 c = input()
 h = ""
+szó = " "
 for i in range(len(c)-1,-1,-1):
     h = h+c[i]
+for i in c.split(" "):
+    print(i)
 
 print(h)
+
+if c == h:
+    print(" ",True,",palindrom")
+else:
+    print(" ",False,",nem palindrom")
