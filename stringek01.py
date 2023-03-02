ee857ccd382f81aa0b88dfe0cb51b80859db4e40
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-03-02 09:14:14 +0100

    2023.03.02 FONTOS!

---
+++ stringek01.py
@@ -0,0 +1,25 @@
+s1='hsgdkA'
+s2="234567654321567"
+s3=''
+if len(s2) > len(s2):
+    for i,j in zip(s1,s2[:len(s2)],s2):
+        s3 = s3+i+j
+        s3 = s3+s1[len(s2):]
+
+
+
+
+
+
+
+
+
+#szoveg = input()
+#db = 0
+#szamok = "0123456789"
+#for i in szoveg:
+#    if i in szamok:
+#        db = db+1
+#        print(i)
+#
+#print(db)
