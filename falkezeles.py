4f1a13fdb1d9c222cabda5df802a8acb02b9f76d
FoszlerDanielA <foszler.daniel.alexander.tech2021d@bolyaimovar.com>
2023-03-16 09:40:17 +0100

    2023.03.16 Nézd ÁT!

---
+++ falkezeles.py
@@ -0,0 +1,24 @@
+himnusz = []
+
+with open("himnusz.txt", "r", encoding="utf-8") as file:
+    for sor in file:
+        himnusz.append(sor.strip())
+
+print(himnusz)
+print(len(himnusz))
+db = 0
+for i in himnusz:
+    if i != "":
+        db = db+1
+print(db-1)
+
+versszakok = []
+versszak = ""
+for i in himnusz:
+    if i == "":
+        versszakok.append(versszak)
+        versszak = ""
+    else:
+        versszak = versszak+i+"\n"
+
+print(versszakok)
